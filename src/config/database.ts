73ba981ac80433b0b343a5cf3a6ce5cd673a2141
Francis Uzoigwe <kossyuzoigwe@gmail.com>
2023-07-14 01:46:08 +0100

    Changes

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,6 +3,6 @@ import mongoose from "mongoose";
 const MongoString: string = process.env.APPLICATION_STRING! 
 export const Mongo = () => {
     mongoose.connect(MongoString).then(() => {
-        console.log("Database connsection established successfully");
+        console.log("Database connection established successfully");
     })
 }
